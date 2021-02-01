da3b44363784ac63c87a5eb8d768d3db3248c89d
M.Khuram Iqbal <khurramkiiman@gmail.com>
2021-02-02 01:02:56 +0500

    change back end url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-//export const baseUrl = "https://customer-repo.herokuapp.com"; //Must match the heroku url
-export const baseUrl = "http://localhost:5000";
+// export const baseUrl = "http://localhost:5000";
+export const baseUrl = "https://josibackend.herokuapp.com";
 export default axios.create({
   baseURL: `${baseUrl}/api`
 });
